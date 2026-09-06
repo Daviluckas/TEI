9039edd2c2e053137ec53d441ed110575001ea60
kchristian08 <christiankallil08@gmail.com>
2026-09-06 15:13:25 -0300

    finalizando interatividade

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const version = '0.0.1';
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
@@ -15,7 +17,17 @@ void main(List<String> arguments) {
 }
 
 void searchWikipedia(List<String>? arguments) {
-  print('searchWikipedia received arguments: $arguments');
+  final String articleTittle;
+
+  if (arguments == null || arguments.isEmpty){
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
 
 void printUsage() {
